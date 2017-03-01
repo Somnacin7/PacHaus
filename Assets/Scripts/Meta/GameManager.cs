e58582f22ec3d5f612637f5138079605c5277beb
Somn <somnacin7@gmail.com>
2017-03-01 01:27:36 -0500

    add persistent score and lives

--- Assets/Scripts/Meta/GameManager.cs
+++ Assets/Scripts/Meta/GameManager.cs
@@ -3,14 +3,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
+
+    public Text livesText;
+    public string livesPrefix = "";
+
+    public int numberOfLives = 3;
+    private int currentLives = 0;
+
+    private int count = 0;
 
     private static GameManager _instance;
 
     public static GameManager Instance { get { return _instance; } }
-
-    public Vector2 playerInitialPosition = Vector2.zero;
 
     private ScoreManager scoreManager;
 
@@ -18,27 +25,74 @@ public class GameManager : MonoBehaviour {
     private DeathEvents playerDeathEvents = null;
     private PacdotPickup playerPacdotPickupEvents = null;
 
-    public void Awake() {
+    void Start() {
+        Setup();
+        currentLives = numberOfLives;
+    }
+
+    void Awake() {
         // If there is an instance and it isn't this one, then commit suicide
         if (_instance != null && _instance != this) {
             Destroy(gameObject);
         } else {
             _instance = this;
         }
+    }
 
+    void Setup() {
         scoreManager = GetComponent<ScoreManager>();
+        if (scoreManager == null) {
+            Debug.LogWarning("No score manager on game manger");
+        }
+
         player = GameObject.FindGameObjectWithTag("Player");
         if (player == null) {
             throw new Exception("Error: no player was found in the scene");
         } else {
-            playerDeathEvents        = player.GetComponent<DeathEvents>();
+            playerDeathEvents = player.GetComponent<DeathEvents>();
             playerPacdotPickupEvents = player.GetComponent<PacdotPickup>();
         }
+
     }
 
     void OnEnable() {
+        RegisterEvents();
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        SceneManager.sceneUnloaded += OnSceneUnloaded;
+    }
+
+    void OnDisable() {
+        DeregisterEvents();
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+        SceneManager.sceneUnloaded -= OnSceneUnloaded;
+    }
+
+    void OnSceneLoaded(Scene prevScene, LoadSceneMode mode) {
+        Debug.Log("count: " + ++count);
+        Setup();
+        RegisterEvents();
+    }
+
+    void OnSceneUnloaded(Scene prevScene) {
+        currentLives--;
+        DeregisterEvents();
+    }
+
+    void OnPlayerDeath() {
+        if (currentLives > 0) {
+            SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+        } else {
+            Time.timeScale = 0;
+        }
+    }
+
+    void OnPlayerPacdotPickup() {
+        scoreManager.AddScore(5.0f);
+    }
+
+    void RegisterEvents() {
         if (playerDeathEvents != null) {
-            playerDeathEvents.OnDeath            += OnPlayerDeath;
+            playerDeathEvents.OnDeath += OnPlayerDeath;
         }
 
         if (playerPacdotPickupEvents != null) {
@@ -46,9 +100,9 @@ public class GameManager : MonoBehaviour {
         }
     }
 
-    void OnDisable() {
-        if (playerDeathEvents != null) {
-            playerDeathEvents.OnDeath            -= OnPlayerDeath;
+    void DeregisterEvents() {
+        if(playerDeathEvents != null) {
+            playerDeathEvents.OnDeath -= OnPlayerDeath;
         }
 
         if (playerPacdotPickupEvents != null) {
@@ -56,11 +110,7 @@ public class GameManager : MonoBehaviour {
         }
     }
 
-    void OnPlayerDeath() {
-        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
-    }
-
-    void OnPlayerPacdotPickup() {
-        scoreManager.AddScore(5.0f);
+    void Update() {
+        livesText.text = livesPrefix + currentLives;
     }
 }
--- Assets/Scripts/Meta/ScoreManager.cs
+++ Assets/Scripts/Meta/ScoreManager.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ScoreManager : MonoBehaviour {
 
@@ -13,8 +15,44 @@ public class ScoreManager : MonoBehaviour {
 
     public float currentScore;
 
+    private static ScoreManager _instance = null;
+    public static ScoreManager Instance {  get { return _instance; } }
+
+    // Start() is called only once per script (regardless of LoadScene())
     void Start() {
         currentScore = 0f;
+    }
+
+    void Awake() {
+        if (_instance != null && _instance != this) {
+            Destroy(gameObject);
+        } else {
+            _instance = this;
+        }
+        Setup();
+    }
+
+    void Setup() {
+
+        Debug.Log(scoreText);
+        if (scoreText == null) {
+            var go = GameObject.FindGameObjectWithTag("scoreText");
+            var text = go.GetComponent<Text>();
+
+            if (text != null) {
+                scoreText = text;
+            } else {
+                throw new Exception("No Score Text Found");
+            }
+        }
+    }
+
+    void OnSceneLoaded(Scene prevScene, LoadSceneMode mode) {
+        Setup();
+    }
+
+    void OnSceneUnloaded(Scene prevScene) {
+        Setup();
     }
 
     void Update() {
@@ -28,6 +66,14 @@ public class ScoreManager : MonoBehaviour {
 
     public void AddScore(float scoreToAdd) {
         currentScore += scoreToAdd;
+
+        if (OnScoreChange != null) {
+            OnScoreChange();
+        }
+    }
+
+    public void SetScore(float newScore) {
+        currentScore = newScore;
 
         if (OnScoreChange != null) {
             OnScoreChange();
